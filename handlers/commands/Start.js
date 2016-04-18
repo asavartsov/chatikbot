cf35e2715de3f285bf27d6f2d56ec146cfd656d9
Alexey Savartsov <a.savartsov@infocom-s.ru>
2016-04-18 12:26:23 +0300

    Режим форматирования HTML

--- handlers/commands/Start.js
+++ handlers/commands/Start.js
@@ -8,12 +8,12 @@ function Start() {
 
 Start.prototype.respond = function (isDirect, text, message, context) {
     if (text == "/start" || (isDirect && /привет/i.test(text))) {
-        var message = "Привет! Я бот и у меня установлены модули:\r\n\r\n";
+        var message = "Привет! Мои модули:\r\n\r\n";
 
         for (var kCmd in context.commands) {
             if (context.commands.hasOwnProperty(kCmd)) {
                 var cmd = context.commands[kCmd];
-                message = message + '*' + cmd.name + '*\r\n' + cmd.description + '\r\n';
+                message = message + '<b>' + cmd.name + '</b>\r\n' + cmd.description + '\r\n';
             
                 if (cmd.help && Object.keys(cmd.help).length > 0) 
                 {
@@ -21,7 +21,7 @@ Start.prototype.respond = function (isDirect, text, message, context) {
                     
                     for (var kHelp in cmd.help) {
                         if (cmd.help.hasOwnProperty(kHelp)) {
-                            message = message + "  _бот, " + kHelp + '_: ' + cmd.help[kHelp] + '\r\n';
+                            message = message + "  <i>бот, " + kHelp + '</i>: ' + cmd.help[kHelp] + '\r\n';
                         }
                     }
                 }
